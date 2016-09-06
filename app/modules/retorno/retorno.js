94d9d433d4ac7f57c25b61b5903a2115d90618ea
Daniel Ordonez <da.ordonez11@uniandes.edu.co>
2016-09-06 09:15:35 -0500

    RentPlace FE crud

--- app/modules/retorno/retorno.js
+++ app/modules/retorno/retorno.js
@@ -10,13 +10,35 @@ angular.module('b4f.retorno', ['ngRoute', 'ngStorage'])
     }])
 
 .controller('PlaceCtrl', ['$scope', '$http', '$localStorage', '$location', function ($scope, $http, $localStorage, $location) {
+    $scope.editMode=false;
+    $scope.editPlace = function(place){
+        console.log(place);
+         $http({
+        method: 'GET',
+        url: 'http://bikes4freeg5.herokuapp.com/rentplace/'+place.id,
+        headers: {
+            Authorization: auth
+        }
+    }).then(function successCallback(response) {
+        $scope.newPlace = response.data;
+        $scope.newPlace.$oid = place.id;
+        console.log(response.data);
+        $scope.editMode=true;
+             return response;
+             
+    }, function errorCallback(response)  {
+        console.log("error finding one place: ");
+        console.log(response);
+    })
+    }
 
     var auth = $localStorage.userInfo != null && $localStorage.userInfo != undefined ? $localStorage.userInfo.token : undefined;
 
 
-    $http({
+    $scope.fetchPlaces = function(){
+        $http({
         method: 'GET',
-        url: 'http://bikes4freeg5.herokuapp.com/rentplace',
+        url: 'http://bikes4freeg5.herokuapp.com/rentplace/',
         headers: {
             Authorization: auth
         }
@@ -24,9 +46,13 @@ angular.module('b4f.retorno', ['ngRoute', 'ngStorage'])
         $scope.places = response.data;
         console.log(response.data);
     }, function errorCallback(response)  {
-
+        console.log("error places: ");
+        console.log(response);
     })
 
+    }
+    $scope.fetchPlaces();
+    
 
     $scope.addPlace = function () {
         $scope.addingPlace = true;
@@ -40,6 +66,7 @@ angular.module('b4f.retorno', ['ngRoute', 'ngStorage'])
     $scope.cancel = function () {
         $scope.addingPlace = false;
         $scope.newPlace = undefined;
+        $scope.editMode = false;
     }
 
     $scope.savePlace = function () {
@@ -57,7 +84,49 @@ angular.module('b4f.retorno', ['ngRoute', 'ngStorage'])
         }).finally(function () {
             $scope.addingPlace = false;
             $scope.newPlace = undefined;
+            $scope.editMode = false;
+            $scope.fetchPlaces();
         });
     }
+     $scope.saveEditedPlace = function () {
+        $http({
+            method: 'PUT',
+            url: 'http://bikes4freeg5.herokuapp.com/rentplace/'+$scope.newPlace.id,
+            headers: {
+                Authorization: auth
+            },
+            data: JSON.stringify($scope.newPlace)
+        }).then(function successCallback(response) {
+            console.log(response.data);
+        }, function errorCallback(response)  {
+
+        }).finally(function () {
+            $scope.addingPlace = false;
+            $scope.newPlace = undefined;
+            $scope.editMode = false;
+            $scope.fetchPlaces();
+        });
+    }
+    $scope.deletePlace = function (place) {
+        
+        $http({
+        method: 'DELETE',
+        url: 'http://bikes4freeg5.herokuapp.com/rentplace/'+place.id,
+        headers: {
+            Authorization: auth
+        }
+            }).then(function successCallback(response) {
+                 console.log("Delete succeded ");
+                $scope.editMode=false;
+                $scope.fetchPlaces();
+                     return response;
+
+            }, function errorCallback(response)  {
+                console.log("error deleting one place: ");
+                console.log(response);
+            })    
+
+            };
+    
 
     }]);
