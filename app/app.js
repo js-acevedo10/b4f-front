6dd1685115796229b4074a9169c6fb8757d99ebf
Juan Santiago Acevedo <js.acevedo10@uniandes.edu.co>
2016-09-06 14:54:14 -0500

    Navbar

--- app/app.js
+++ app/app.js
@@ -10,14 +10,22 @@ angular.module('b4f', [
     'b4f.bikes',
     'b4f.retorno',
     'b4f.penalty'
-]).
-config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+])
+.controller('NavController', ['$scope', '$localStorage', '$location', function($scope, $localStorage, $location) {
+    $scope.openNav = function () {
+        document.getElementById("mySidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
+
+    $scope.closeNav = function () {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
+}])
+.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
     $locationProvider.hashPrefix('!');
 
     $routeProvider.otherwise({
         redirectTo: '/login'
     });
-
-
-
 }]);
