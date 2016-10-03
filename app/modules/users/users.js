4b2267961a106914268c97acc247759d31e2ffad
Daniel Soto <dansotorey@gmail.com>
2016-10-03 12:27:17 -0500

    Users

--- app/modules/users/users.js
+++ app/modules/users/users.js
@@ -49,17 +49,17 @@ angular.module('b4f.users', ['ngRoute', 'ngStorage'])
 
         })
         
-//        $scope.loading = $http({
-//            method: 'GET',
-//            url: 'http://bikes4freeg5.herokuapp.com/client',
-//            //        url: 'http://localhost:8080/client',
-//            headers: {
-//                Authorization: auth
-//            }
-//        }).then(function successCallback(response) {
-//            $scope.clients = response.data;
-//            console.log(response.data);
-//        }, function errorCallback(response)  {
-//
-//        })
+        $scope.loading = $http({
+            method: 'GET',
+            url: 'http://bikes4freeg5.herokuapp.com/admin',
+            //        url: 'http://localhost:8080/client',
+            headers: {
+                Authorization: auth
+            }
+        }).then(function successCallback(response) {
+            $scope.admins = response.data;
+            console.log(response.data);
+        }, function errorCallback(response)  {
+
+        })
     }]);
