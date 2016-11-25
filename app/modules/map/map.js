5831c6b5c129204c83500d8d22a0ee6f8ca86a4c
Daniel Ordonez <da.ordonez11@uniandes.edu.co>
2016-11-25 01:34:39 -0500

    RQ 11 - mapa interactivo con info de los return points

--- app/modules/map/map.js
+++ app/modules/map/map.js
@@ -19,13 +19,18 @@ angular.module('b4f.map', ['ngRoute', 'ngStorage','uiGmapgoogle-maps'])
         });
     }])
 
-.controller('MapCtrl', ['$scope', '$http', '$localStorage', '$location', '$base64', function ($scope, $http, $localStorage, $location, $base64) {
+.controller('MapCtrl', ['$scope', '$http', '$localStorage', '$location', '$base64','uiGmapIsReady', function ($scope, $http, $localStorage, $location, $base64, IsReady) {
     $scope.venues =[];
     var auth = $localStorage.userInfo != null && $localStorage.userInfo != undefined ? $base64.decode($localStorage.userInfo[$base64.encode('token')]) : undefined;
     var role = $localStorage.userInfo != null && $localStorage.userInfo != undefined ? $base64.decode($localStorage.userInfo[$base64.encode('role')]) : undefined;
     var id = $localStorage.userInfo != null && $localStorage.userInfo != undefined ? $base64.decode($localStorage.userInfo[$base64.encode('id')]) : undefined;
-   $scope.map = { center: { latitude: 4.71, longitude: -74.07 }, zoom: 8 };
-
+      
+   $scope.map = { center: { latitude: 4.71, longitude: -74.07 }, zoom: 8 ,markers: [],};
+  IsReady.promise()                     // this gets all (ready) map instances - defaults to 1 for the first map
+    .then(function(instances) {                 // instances is an array object
+        $scope.datMap = instances[0].map;            // if only 1 map it's found at index 0 of array
+        $scope.fetchVenues();       // this function will only be applied on initial map load (once ready)
+    });
  $scope.fetchVenues = function () {
         $scope.bikesPromise = $http({
             method: 'GET',
@@ -35,13 +40,27 @@ angular.module('b4f.map', ['ngRoute', 'ngStorage','uiGmapgoogle-maps'])
             }
         }).then(function successCallback(response) {
             $scope.venues = response.data;
-            console.log(response.data);
+            $scope.venues.forEach(function(entry){
+                if(entry.coordinates!==undefined) {
+                         var marker = {
+                                id: entry.id,
+                                coords: {
+                                    latitude: entry.coordinates.latitude,
+                                    longitude: entry.coordinates.longitude
+                                },
+                                name: entry.name,
+                                address: entry.address
+                            };
+                        $scope.map.markers.push(marker);
+                    //    $scope.$apply();
+                    }
+            });
             
         }, function errorCallback(response)  {
 
         })
     }
-    $scope.fetchVenues();
+    
 
 
     }]);
