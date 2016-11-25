655b7ffc7f0daa31ba7985ecae44e01d17958639
Daniel Ordonez <da.ordonez11@uniandes.edu.co>
2016-11-25 03:59:57 -0500

    R9 - Almost done, just need to sum bikes, penalties and users

--- app/modules/dashboard/dashboard.js
+++ app/modules/dashboard/dashboard.js
@@ -101,7 +101,7 @@ angular.module('b4f.dashboard', ['ngRoute', 'ngStorage', 'chart.js'])
     $scope.labels2 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
     $scope.series2 = ['Trip Duration'];
     $scope.data2 = [
-    [65, 59, 80, 81, 56, 55, 100, 59, 80, 81, 56, 55, 100, 59, 80, 81, 56, 55, 100, 59, 80, 81, 56]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   ];
     $scope.datasetOverride2 = [{
         yAxisID: 'y-axis-1'
@@ -124,7 +124,6 @@ angular.module('b4f.dashboard', ['ngRoute', 'ngStorage', 'chart.js'])
     }
 
     $scope.donate = function () {
-        console.log($scope.donacion.monto);
         $scope.fetchReserves = $http({
             method: 'POST',
             url: 'http://bikes4freeg5.herokuapp.com/donations/',
@@ -166,5 +165,64 @@ angular.module('b4f.dashboard', ['ngRoute', 'ngStorage', 'chart.js'])
             $scope.data2 = daticos2;
         });
     }
-
+    
+    $scope.generateReport = function(){
+        var completeObj={
+            venues: $scope.venues,
+            rentals: $scope.rentals,
+            donations: $scope.donaciones,
+         //   bikes: $scope.bikes,
+        //    penalties: $scope.penalties
+        }
+        var items = completeObj.venues
+        var replacer = (key, value) => value === null||value===undefined ? '' : value // specify how you want to handle null values here
+        var header = Object.keys(items[0])
+        var csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+        csv.unshift(header.join(','))
+        var csvText ="Venues\r\n"+ csv.join('\r\n')
+        csvText += '\r\n\r\n'
+        csvText += 'Rentals\r\n'
+        var items = completeObj.rentals
+        var header = Object.keys(items[0])
+        csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+        csv.unshift(header.join(','))
+        csvText += csv.join('\r\n')
+        csvText += '\r\n\r\n'
+        csvText += 'Donations\r\n'
+        var items = completeObj.donations
+        var header = Object.keys(items[0])
+        csv = items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+        csv.unshift(header.join(','))
+        csvText += csv.join('\r\n')
+        csvText += '\r\n\r\n'
+        
+        downloadCSV(csvText)
+         //Generate a file name
+ 
+    }
+    function downloadCSV(csv){
+           var fileName = "Bikes4FreeReport";
+ 
+    
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(csv);
+    
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension    
+    
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");    
+    link.href = uri;
+    
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+    
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    }
 }]);
