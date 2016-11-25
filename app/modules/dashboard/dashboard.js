4b7e2eee31b23dbfcdff2fcce1592d2335b35983
Juan Santiago Acevedo <js.acevedo10@uniandes.edu.co>
2016-11-25 00:16:07 -0500

    Donaciones DONE

--- app/modules/dashboard/dashboard.js
+++ app/modules/dashboard/dashboard.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('b4f.dashboard', ['ngRoute', 'ngStorage','chart.js'])
+angular.module('b4f.dashboard', ['ngRoute', 'ngStorage', 'chart.js'])
 
 .config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/dashboard', {
@@ -19,8 +19,11 @@ angular.module('b4f.dashboard', ['ngRoute', 'ngStorage','chart.js'])
 .controller('DashboardCtrl', ['$scope', '$http', '$localStorage', '$location', '$base64', function ($scope, $http, $localStorage, $location, $base64) {
 
     var auth = $localStorage.userInfo != null && $localStorage.userInfo != undefined ? $base64.decode($localStorage.userInfo[$base64.encode('token')]) : undefined;
+    var id = $localStorage.userInfo != null && $localStorage.userInfo != undefined ? $base64.decode($localStorage.userInfo[$base64.encode('id')]) : undefined;
+    $scope.role = $localStorage.userInfo != null && $localStorage.userInfo != undefined ? $base64.decode($localStorage.userInfo[$base64.encode('role')]) : undefined;
+    $scope.donacionMode = false;
 
-     $scope.fetchReserves = function () {
+    $scope.fetchReserves = function () {
         $scope.bikesPromise = $http({
             method: 'GET',
             url: 'http://bikes4freeg5.herokuapp.com/rental',
@@ -29,13 +32,27 @@ angular.module('b4f.dashboard', ['ngRoute', 'ngStorage','chart.js'])
             }
         }).then(function successCallback(response) {
             $scope.rentals = response.data;
-            console.log(response.data);
-              $scope.loadTripDurationData();
+            $scope.loadTripDurationData();
         }, function errorCallback(response)  {
 
         })
     }
     $scope.fetchReserves();
+    
+    $scope.fetchDonaciones = function () {
+        $scope.bikesPromise = $http({
+            method: 'GET',
+            url: 'http://bikes4freeg5.herokuapp.com/donations',
+            headers: {
+                Authorization: auth
+            }
+        }).then(function successCallback(response) {
+            $scope.donaciones = response.data;
+        }, function errorCallback(response)  {
+
+        })
+    };
+    $scope.fetchDonaciones();
     $scope.fetchVenues = function () {
         $scope.bikesPromise = $http({
             method: 'GET',
@@ -45,7 +62,6 @@ angular.module('b4f.dashboard', ['ngRoute', 'ngStorage','chart.js'])
             }
         }).then(function successCallback(response) {
             $scope.venues = response.data;
-            console.log(response.data);
         }, function errorCallback(response)  {
 
         })
@@ -58,67 +74,97 @@ angular.module('b4f.dashboard', ['ngRoute', 'ngStorage','chart.js'])
     $scope.goToPoints = function () {
         $location.path("/rentplace");
     }
-    $scope.labels = ["0","1", "2", "3", "4", "5", "6"];
-  $scope.series = ['Trip Duration'];
-  $scope.data = [
+    $scope.labels = ["0", "1", "2", "3", "4", "5", "6"];
+    $scope.series = ['Trip Duration'];
+    $scope.data = [
     [0, 0, 0, 0, 0, 0, 0]
   ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
-  $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
+    $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+    };
+    $scope.datasetOverride = [{
+        yAxisID: 'y-axis-1'
+    }];
+    $scope.options = {
+        scales: {
+            yAxes: [
+                {
+                    id: 'y-axis-1',
+                    type: 'linear',
+                    display: true,
+                    position: 'left'
         }
       ]
+        }
     }
-  }
-  
-    $scope.labels2 = ["0","1", "2", "3", "4", "5", "6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"];
-  $scope.series2 = ['Trip Duration'];
-  $scope.data2 = [
-    [65, 59, 80, 81, 56, 55,100,59, 80, 81, 56, 55,100,59, 80, 81, 56, 55,100,59, 80, 81, 56]
+
+    $scope.labels2 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"];
+    $scope.series2 = ['Trip Duration'];
+    $scope.data2 = [
+    [65, 59, 80, 81, 56, 55, 100, 59, 80, 81, 56, 55, 100, 59, 80, 81, 56, 55, 100, 59, 80, 81, 56]
   ];
-  $scope.datasetOverride2 = [{ yAxisID: 'y-axis-1' }];
-  $scope.options2 = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
+    $scope.datasetOverride2 = [{
+        yAxisID: 'y-axis-1'
+    }];
+    $scope.options2 = {
+        scales: {
+            yAxes: [
+                {
+                    id: 'y-axis-1',
+                    type: 'linear',
+                    display: true,
+                    position: 'left'
         }
       ]
+        }
     }
-  }
-  
-  $scope.loadTripDurationData = function(){
-          var daticos = [0,0,0,0,0,0,0];
-        var daticos2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-          $scope.rentals.forEach(function(entry) {
-              var dateTal = new Date(entry.rentDate);
-              var fechaI = Date.parse(entry.rentDate);
-              var fechaF = Date.parse(entry.modifiedAt);
-              var restaMil = fechaF-fechaI;
-              var restaSegs = restaMil/1000;
-              var restaMins = restaSegs/60;
-              var restaH =restaMins/60;
-              var intHor = Math.round(restaH);
-              if(intHor<7)
-                  {
-                      daticos[intHor]=daticos[intHor]+1;
-                  }
-              daticos2[dateTal.getHours()]=daticos2[dateTal.getHours()]+1;
-              $scope.data=daticos;
-              $scope.data2=daticos2;
-          });
-  }
+
+    $scope.showDonacionMode = function () {
+        $scope.donacionMode = !$scope.donacionMode;
+    }
+
+    $scope.donate = function () {
+        console.log($scope.donacion.monto);
+        $scope.fetchReserves = $http({
+            method: 'POST',
+            url: 'http://bikes4freeg5.herokuapp.com/donations/',
+//            url: 'http://localhost:8080/donations',
+            headers: {
+                Authorization: auth
+            },
+            data: {
+                fecha: new Date().getTime(),
+                amount: $scope.donacion.monto,
+                userId: id
+            }
+        }).then(function successCallback(response) {
+            $scope.donacionMode = false;
+            $('.alertaDonacion').html("<strong>Thank you for donating.</strong>");
+            $scope.donaciones = response.data;
+        }, function errorCallback(response)  {
+            console.error(response);
+        })
+    }
+
+    $scope.loadTripDurationData = function () {
+        var daticos = [0, 0, 0, 0, 0, 0, 0];
+        var daticos2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        $scope.rentals.forEach(function (entry) {
+            var dateTal = new Date(entry.rentDate);
+            var fechaI = Date.parse(entry.rentDate);
+            var fechaF = Date.parse(entry.modifiedAt);
+            var restaMil = fechaF - fechaI;
+            var restaSegs = restaMil / 1000;
+            var restaMins = restaSegs / 60;
+            var restaH = restaMins / 60;
+            var intHor = Math.round(restaH);
+            if (intHor < 7) {
+                daticos[intHor] = daticos[intHor] + 1;
+            }
+            daticos2[dateTal.getHours()] = daticos2[dateTal.getHours()] + 1;
+            $scope.data = daticos;
+            $scope.data2 = daticos2;
+        });
+    }
 
 }]);
